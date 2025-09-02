9c35e51b689829445c25a0e5a17125c8fbf72ce7
Vasishta S <vasishta.lfc@gmail.com>
2025-09-02 23:04:20 +0530

    Minor changes

--- commands/setup_commands.py
+++ commands/setup_commands.py
@@ -1,8 +1,10 @@
 import discord
 from .analysis_commands import setup_analysis_commands
 from .coin_commands import setup_coin_commands
+from .graph_commands import setup_graph_commands
 
 
 def setup_commands(tree: discord.app_commands.CommandTree, guild_id: int):
     setup_coin_commands(tree=tree, guild_id=guild_id)
     setup_analysis_commands(tree=tree, guild_id=guild_id)
+    setup_graph_commands(tree=tree, guild_id=guild_id)
